45de049d8b0d2c30effe8617f95df6e2af75d6bc
zizhuang.lcq <zizhuang.lcq@alipay.com>
2017-06-03 11:11:55 +0800

    finish project

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,22 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+        std::cout << "Invalid estimation or groud_truth data " << std::endl;
+    }
+
+    for (unsigned int i = 0; i < estimations.size(); i++) {
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    rmse /= estimations.size();
+    rmse = rmse.array().sqrt();
+
+    return rmse;
 }
